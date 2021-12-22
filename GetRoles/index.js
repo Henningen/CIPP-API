6801999908319c5f53dfff48e7c6c90d0aad5762
henning@idrift.no <henning@idrift.no>
2021-12-22 17:49:50 +0100

    Api changes for auth

---
+++ GetRoles/index.js
@@ -0,0 +1,42 @@
+const fetch = require('node-fetch').default;
+
+// add role names to this object to map them to group ids in your AAD tenant
+const roleGroupMappings = {
+    'admin': 'a86753b8-e17d-4d83-a57b-ce577629c481',
+	'editor': '6002041b-4d3e-49f8-a9b2-ed0019d10b90',
+    'reader': '4dc6e90f-c776-4b5b-a32f-000969e6bda1'
+};
+
+module.exports = async function (context, req) {
+    const user = req.body || {};
+    const roles = [];
+    
+    for (const [role, groupId] of Object.entries(roleGroupMappings)) {
+        if (await isUserInGroup(groupId, user.accessToken)) {
+            roles.push(role);
+        }
+    }
+
+    context.res.json({
+        roles
+    });
+}
+
+async function isUserInGroup(groupId, bearerToken) {
+    const url = new URL('https://graph.microsoft.com/v1.0/me/memberOf');
+    url.searchParams.append('$filter', `id eq '${groupId}'`);
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${bearerToken}`
+        },
+    });
+
+    if (response.status !== 200) {
+        return false;
+    }
+
+    const graphResponse = await response.json();
+    const matchingGroups = graphResponse.value.filter(group => group.id === groupId);
+    return matchingGroups.length > 0;
+}
